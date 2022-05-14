100448fa4dd2b4dbd6b1da7c0b26c46132fcfbd5
PriyanshuG13 <priyanshu.gupta1312@gmail.com>
2022-05-14 18:49:51 +0530

    eslint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import type {Node} from 'react';
 import {
     SafeAreaView,
     ScrollView,
@@ -19,7 +18,7 @@ import {
     ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
+const Section = ({children, title}) => {
     const isDarkMode = useColorScheme() === 'dark';
     return (
             <View style={styles.sectionContainer}>

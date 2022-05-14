cdb91378bc511badf2b78019875f5d10cc14aa02
PriyanshuG13 <priyanshu.gupta1312@gmail.com>
2022-05-14 19:05:40 +0530

    eslint

--- src/index.js
+++ src/index.js
@@ -18,29 +18,31 @@ import {
     ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}) => {
+const Section = ({ children, title }) => {
     const isDarkMode = useColorScheme() === 'dark';
     return (
-            <View style={styles.sectionContainer}>
-                <Text
-                        style={[
-                            styles.sectionTitle,
-                            {
-                                color: isDarkMode ? Colors.white : Colors.black,
-                            },
-                        ]}>
-                    {title}
-                </Text>
-                <Text
-                        style={[
-                            styles.sectionDescription,
-                            {
-                                color: isDarkMode ? Colors.light : Colors.dark,
-                            },
-                        ]}>
-                    {children}
-                </Text>
-            </View>
+        <View style={styles.sectionContainer}>
+            <Text
+                style={[
+                    styles.sectionTitle,
+                    {
+                        color: isDarkMode ? Colors.white : Colors.black,
+                    },
+                ]}
+            >
+                {title}
+            </Text>
+            <Text
+                style={[
+                    styles.sectionDescription,
+                    {
+                        color: isDarkMode ? Colors.light : Colors.dark,
+                    },
+                ]}
+            >
+                {children}
+            </Text>
+        </View>
     );
 };
 
@@ -52,33 +54,30 @@ const App: () => Node = () => {
     };
 
     return (
-            <SafeAreaView style={backgroundStyle}>
-                <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-                <ScrollView
-                        contentInsetAdjustmentBehavior="automatic"
-                        style={backgroundStyle}>
-                    <Header />
-                    <View
-                            style={{
-                                backgroundColor: isDarkMode ? Colors.black : Colors.white,
-                            }}>
-                        <Section title="Step One">
-                            Edit <Text style={styles.highlight}>App.js</Text> to change this
-                            screen and then come back to see your edits.
-                        </Section>
-                        <Section title="See Your Changes">
-                            <ReloadInstructions />
-                        </Section>
-                        <Section title="Debug">
-                            <DebugInstructions />
-                        </Section>
-                        <Section title="Learn More">
-                            Read the docs to discover what to do next:
-                        </Section>
-                        <LearnMoreLinks />
-                    </View>
-                </ScrollView>
-            </SafeAreaView>
+        <SafeAreaView style={backgroundStyle}>
+            <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+            <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
+                <Header />
+                <View
+                    style={{
+                        backgroundColor: isDarkMode ? Colors.black : Colors.white,
+                    }}
+                >
+                    <Section title="Step One">
+                        Edit <Text style={styles.highlight}>App.js</Text> to change this screen and
+                        then come back to see your edits.
+                    </Section>
+                    <Section title="See Your Changes">
+                        <ReloadInstructions />
+                    </Section>
+                    <Section title="Debug">
+                        <DebugInstructions />
+                    </Section>
+                    <Section title="Learn More">Read the docs to discover what to do next:</Section>
+                    <LearnMoreLinks />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 };
 

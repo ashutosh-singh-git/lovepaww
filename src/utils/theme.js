7c6a176694bb98f1d42024f997042b2509499284
PriyanshuG13 <priyanshu.gupta1312@gmail.com>
2022-06-01 16:44:37 +0530

    updated palette

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -9,17 +9,25 @@ const GREY = {
     700: '#454F5B',
     800: '#212B36',
     900: '#161C24',
+    1: '#000000'
 };
 
 const PRIMARY = {
-    light: '#bcdfe5',
+    lighter: '#c4e0e5',
+    light: '#a0cdd5',
     main: '#88c1cb',
-    dark: '#508791',
+    dark: '#6d9aa2',
+    darker: '#446166',
+    contrastText: '#fff'
 };
 
 const SECONDARY = {
-    lighter: '#ffeac6',
+    lighter: '#e4cb9d',
+    light: '#d9b676',
     main: '#c9963b',
+    dark: '#a1782f',
+    darker: '#654b1e',
+    contrastText: '#fff'
 };
 
 const INFO = {
@@ -28,7 +36,7 @@ const INFO = {
     main: '#1890FF',
     dark: '#0C53B7',
     darker: '#04297A',
-    contrastText: '#fff',
+    contrastText: '#fff'
 };
 const SUCCESS = {
     lighter: '#E9FCD4',
@@ -36,7 +44,7 @@ const SUCCESS = {
     main: '#54D62C',
     dark: '#229A16',
     darker: '#08660D',
-    contrastText: GREY[800],
+    contrastText: GREY[800]
 };
 const WARNING = {
     lighter: '#FFF7CD',
@@ -44,7 +52,7 @@ const WARNING = {
     main: '#FFC107',
     dark: '#B78103',
     darker: '#7A4F01',
-    contrastText: GREY[800],
+    contrastText: GREY[800]
 };
 const ERROR = {
     lighter: '#FFE7D9',
@@ -52,12 +60,12 @@ const ERROR = {
     main: '#FF4842',
     dark: '#B72136',
     darker: '#7A0C2E',
-    contrastText: '#fff',
+    contrastText: '#fff'
 };
 
 export default {
     palette: {
-        common: { black: '#000000', white: '#ffffff' },
+        common: { black: GREY[1], white: GREY[0] },
         primary: PRIMARY,
         secondary: SECONDARY,
         info: INFO,
@@ -69,8 +77,8 @@ export default {
             primary: GREY[800],
             secondary: GREY[600],
             disabled: GREY[500],
-            light: '#ffffff',
-            dark: '#000000',
-        },
-    },
+            light: GREY[0],
+            dark: GREY[1]
+        }
+    }
 };
